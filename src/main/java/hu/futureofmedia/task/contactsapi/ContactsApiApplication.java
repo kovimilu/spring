657204f618c63c1a8ff54d679470616602e42686
Kovacs Milan <kovimilu@gmail.com>
2021-11-26 14:21:32 +0100

    Test

--- src/main/java/hu/futureofmedia/task/contactsapi/ContactsApiApplication.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/ContactsApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContactsApiApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(ContactsApiApplication.class, args);
     }
 }
